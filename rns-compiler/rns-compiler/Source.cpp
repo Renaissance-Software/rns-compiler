069d0436bbe8b2a3286f255b63d0dbe497e62f1c
David <39247043+davidgm94@users.noreply.github.com>
2021-03-06 09:58:54 +0100

    First commit

---
+++ rns-compiler/rns-compiler/Source.cpp
@@ -0,0 +1,5 @@
+#include "Source.h"
+int foo()
+{
+    return 5 + 3;
+}
---
+++ rns-compiler/rns-compiler/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "Source.h"
+
+int main(void)
+{
+    printf("Hello foo%d\n", foo());
+    printf("Hello world\n");
+}
